34f4344e841b424de63bcc1ec78dedf7f60f3ff0
Ezgi Sevi <ezgi.sevi16@gmail.com>
2022-07-29 19:30:57 +0300

    Main Widget Created

--- learning_app/lib/main.dart
+++ learning_app/lib/main.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(EcisevApp());
+}
+
+class EcisevApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello!'),
+    );
+  }
+}
